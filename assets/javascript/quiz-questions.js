8dc0391998c6c42a3087d608214d57fa9ea88acd
Joel Getzke <joelcode2023@gmail.com>
2023-05-21 11:34:44 -0500

    added javascript psedo code

---
+++ assets/javascript/quiz-questions.js
@@ -0,0 +1,23 @@
+var questions = {
+{
+    question: "Which is not a js data type"
+    choices: {'pickles', 'strings', 'booleans', 'integers'}
+    answer: "pickles"
+}
+{
+    question: "Which is not a js data type"
+    choices: {'pickles', 'strings', 'booleans', 'integers'}
+    answer: "pickles"
+}
+{
+    question: "Which is not a js data type"
+    choices: {'pickles', 'strings', 'booleans', 'integers'}
+    answer: "pickles"
+}
+
+}
+
+
+questions[currentQuestion].question
+questions[currentQuestion].choices
+questions[currentQuestion].answer
